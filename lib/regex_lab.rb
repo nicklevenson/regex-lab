a3dc2dff969d8735cc33b487d969924e122c2f09
Nicholas Levenson <nicklevenson@gmail.com>
2020-10-27 09:30:03 -0700

    commit tree 7ee2d07590cb6bc4f3ec942a60318d80d8262d4b

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,13 +1,18 @@
 def starts_with_a_vowel?(word)
-
+    if word[0].match(/[aeiouAEIOU]/)
+        true
+    
+    else
+        false
+    end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+    text.scan(/un[a-zA-Z0-9-]+ing/)
 end
 
 def words_five_letters_long(text)
-
+    text.scan(/[a-zA-Z]{5,}/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
